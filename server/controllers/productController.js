5f3ae0ca750ddd7051fa6faf7c47e794da32c0ec
mukesh sharma <loveysharma4801@gmail.com>
2024-03-03 23:11:04 +0530

    search route implemented

--- server/controllers/productController.js
+++ server/controllers/productController.js
@@ -4,7 +4,43 @@ exports.sayHi =  async (req,res) => {
         status: 'success',
         error:false,
         data:{
-            message: 'Hello to the bakend of pokemon app, this place makes it work'
+            message: 'Hello to the bakend of foodiee app, this place makes it work'
         }
     });
 };
+exports.getAllProducts = async (req,res) =>{
+    try{
+        const products =  await Product.find();
+        return res.status(200).json({
+            status:'success',
+            error:false,
+            products
+        })
+    }
+    catch(err){
+        return res.status(400).json({
+            status:'fail',
+            error:true,
+            message:err.message
+        })
+    }
+}
+exports.searchProducts = async (req,res) =>{
+    try{
+        const searchTermRegex = new RegExp(req.query.q, 'i');
+        const products  = await Product.find({ name: { $regex: searchTermRegex } }).exec();
+        
+        return res.status(200).json({
+            status:'success',
+            error:false,
+            products
+        })
+    }
+    catch(err){
+        return res.status(400).json({
+            status:'fail',
+            error:true,
+            message:err.message
+        })
+    }
+}
--- server/routes/productRoutes.js
+++ server/routes/productRoutes.js
@@ -2,4 +2,7 @@ const express = require('express');
 const productController = require('./../controllers/productController');
 const router = express.Router();
 router.route('/').get(productController.sayHi)
+router.route('/getallproducts').get(productController.getAllProducts);
+router.route('/search').get(productController.searchProducts);
 module.exports = router;
+
